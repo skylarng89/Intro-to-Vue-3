7bd5a295cc96e2a489e5bec001891407f3168b20
Patrick Aziken <onaziken@gmail.com>
2022-12-22 12:31:10 +0100

    List Rendering

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            colours: ['Blue', 'Green', 'Yellow'],
+            footSizes: [
+                { id: 123, size: 'Small'},
+                { id: 234, size: 'Medium'},
+                { id: 345, size: 'Large'},
+                { id: 456, size: 'Extra Large'},
+                { id: 567, size: 'Extra Extra Large'}
+            ]
         }
     }
 })
