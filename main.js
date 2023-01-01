1d477048b6fdda723a5ffd47122e28c594892564
Patrick Aziken <onaziken@gmail.com>
2023-01-02 00:55:31 +0100

    Class & Style Binding

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart++
+        },
+        removeFromCart() {
+            this.cart--;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
